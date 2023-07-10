57d7fae87d4a11f10ddbd01290fa73bdf8c9b0a1
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-07-10 15:37:25 -0300

    feat: add middleware to validate create ticket in router

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { authenticateToken } from "@/middlewares";
+import { authenticateToken, validateBody } from "@/middlewares";
 import { getTicketTypes, getTickets, createTicket } from "@/controllers";
+import { ticketsSchema } from '@/schemas/tickets-schema';
 
 const ticketsRouter = Router();
 
 ticketsRouter
   .all("/*", authenticateToken)
   .get("/types", getTicketTypes)
-  .get("", getTickets)
-  .post("", createTicket);
+  .get("/", getTickets)
+  .post("/", validateBody(ticketsSchema), createTicket);
 
 export { ticketsRouter };
