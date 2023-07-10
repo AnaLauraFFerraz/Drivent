8058d48e4679770f5486f8407948c59f37ecef76
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-07-10 15:38:37 -0300

    feat: add create ticket service

--- src/services/tickets-service/index.ts
+++ src/services/tickets-service/index.ts
@@ -2,6 +2,7 @@ import { notFoundError } from "@/errors";
 import ticketRepository from "@/repositories/ticket-repository/index";
 import enrollmentRepository from "@/repositories/enrollment-repository/index";
 import { TicketStatus } from "@prisma/client";
+import { CreateTicketParams } from '@/protocols';
 
 async function getTicketTypes() {
   const ticketTypes = await ticketRepository.findTicketTypes();
@@ -24,9 +25,27 @@ async function getTicketByUserId(userId: number) {
   return ticket;
 }
 
+async function createTicket(userId: number, ticketTypeId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) {
+    throw notFoundError();
+  }
+
+  const ticketData: CreateTicketParams = {
+    ticketTypeId,
+    enrollmentId: enrollment.id,
+    status: TicketStatus.RESERVED
+  };
+
+  await ticketRepository.createTicket(ticketData);
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+  return ticket;
+}
+
 const ticketService = {
   getTicketTypes,
   getTicketByUserId,
+  createTicket
 };
 
 export default ticketService;
